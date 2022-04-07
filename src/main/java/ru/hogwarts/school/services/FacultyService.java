1c9667cbffa45c7789b063a0063f5fac131d6368
Katsiaryna <katrinkuzukovich@gmail.com>
2022-04-08 00:33:08 +0300

    homework9

--- src/main/java/ru/hogwarts/school/services/FacultyService.java
+++ src/main/java/ru/hogwarts/school/services/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.repository.FacultyRepository;
@@ -12,32 +14,39 @@ import java.util.List;
 public class FacultyService {
 
     private final FacultyRepository facultyRepository;
+    private static final Logger logger = LoggerFactory.getLogger(FacultyService.class);
 
     public FacultyService(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Was invoked method createFaculty");
         return facultyRepository.save(faculty);
     }
 
     public Faculty getFaculty(long id) {
+        logger.info("Was invoked method getFaculty:идентификатор {}", id);
         return facultyRepository.findById(id).get();
     }
 
     public Collection<Faculty> getFacultyByColor(String color) {
+        logger.info("Was invoked method getFacultyByColor:цвет {}", color);
         return facultyRepository.findByColor(color);
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("Was invoked method editFaculty");
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(long id) {
+        logger.info("Was invoked method deleteFaculty: идентификатор {}", id);
         facultyRepository.deleteById(id);
     }
 
     public List<Faculty> findByColorIgnoreCaseOrNameIgnoreCase(String color, String name) {
+        logger.info("Was invoked method findByColorIgnoreCaseOrNameIgnoreCase: цвет  {}, имя {}", color, name);
         return facultyRepository.findByColorIgnoreCaseOrNameIgnoreCase(color, name);
     }
 }
--- src/main/java/ru/hogwarts/school/services/StudentService.java
+++ src/main/java/ru/hogwarts/school/services/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.StudentRepository;
@@ -11,44 +13,54 @@ import java.util.List;
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
+    private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     public Student createStudent(Student student) {
+        logger.info("Was invoked method createStudent");
         return studentRepository.save(student);
     }
 
     public Student getStudent(long id) {
+        logger.info("Was invoked method getStudent: идентификатор {}", id);
         return studentRepository.findById(id).get();
     }
 
     public Collection<Student> getStudensByAge(int age) {
+        logger.info("Was invoked method getStudensByAge: возраст {}", age);
         return studentRepository.findByAge(age);
     }
 
     public Student editStudent(Student student) {
+        logger.info("Was invoked method editStudent");
         return studentRepository.save(student);
     }
 
     public void deleteStudent(long id) {
+        logger.info("Was invoked method deleteStudent: идентификатор {}", id);
         studentRepository.deleteById(id);
     }
 
     public List<Student> findByAgeBetween(int minAge, int maxAge) {
+        logger.info("Was invoked method findByAgeBetween: минимальный возраст {},максимальный возраст {}", minAge, maxAge);
         return studentRepository.findByAgeBetween(minAge, maxAge);
     }
 
     public int getStudentCount() {
+        logger.info("Was invoked method getStudentCount");
         return studentRepository.getStudentCount();
     }
 
     public int getStudentAverageAge() {
+        logger.info("Was invoked method getStudentAverageAge");
         return studentRepository.getStudentAverageAge();
     }
 
     public List<Student> getLastStudents(int count) {
+        logger.info("Was invoked method getLastStudents: количество {}", count);
         return studentRepository.getLastStudents(count);
     }
 
